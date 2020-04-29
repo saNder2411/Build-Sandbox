df3e7f97833fb6f9e899c48c5e63ba097d122d3e
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-29 18:18:39 +0300

    Optimization build for custom browsers.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 class App {
 
-  run = (name = `World`) => {
+  run = async (name = `World`) => {
     console.log(`Hello ${name}`);
   };
 }
 
 const app = new App();
-app.run();
+app.run()
+  .then(() => console.log(`Done`))
+  .catch(() => console.log(`Error`));
