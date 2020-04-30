4c06e6825bdc16ed71f1bfc65b29e1dfff1a93c1
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-30 12:43:09 +0300

    Config Loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
 module.exports = {
-  mode: "development",
+  mode: `development`,
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpeg|jpg|gif|ico)$/,
+        use: [
+          {
+            loader: `file-loader`,
+            options: {
+              outputPath: `images`,
+              name: `[name]-[sha1:hash:7].[ext]`,
+            },
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: `file-loader`,
+            options: {
+              outputPath: `fonts`,
+              name: `[name].[ext]`,
+            },
+          }
+        ]
+      }
+    ]
+  }
 };
